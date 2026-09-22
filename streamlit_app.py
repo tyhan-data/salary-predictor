4e5d3ebabb7731c15b1c2c084768b79ee2201232
Mosairul Alam Tyhan <213462942+tyhan-data@users.noreply.github.com>
2026-09-22 17:08:20 +0600

    update api-url

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://salary-predictor-r2cl.onrender.com/predict"
 
 
 # ---------------- PAGE CONFIG ----------------
